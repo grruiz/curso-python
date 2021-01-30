b0cf486a65ceba3a60b24eb9b61139de43132e54
Rafael Gálvez Ruiz <galvezruizrafa@gmail.com>
2021-01-30 05:33:03 +0100

    Add files via upload

---
+++ 13-paquetes/main.py
@@ -0,0 +1,7 @@
+print("PROBANDO PAQUETES HOLAA RAFAA")
+
+from paquete import pruebas
+from paquete import herramientas
+
+pruebas.probando()
+herramientas.nombreCompleto("Rafa","Galvez")
---
+++ 13-paquetes/paquete/herramientas.py
@@ -0,0 +1,2 @@
+def nombreCompleto(nombre, apellidos):
+    print(f"Nombre: {nombre}, Apellidos: {apellidos}")
